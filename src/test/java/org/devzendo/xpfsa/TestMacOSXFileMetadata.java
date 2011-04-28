e7500699f6cc9f11ed54ecabdd9321cb32e1d014
Matt Gumbley <matt.gumbley@devzendo.org>
2011-04-28 07:59:26 +0100

    Create more test data, test for file status.

--- src/test/java/org/devzendo/xpfsa/TestMacOSXFileMetadata.java
+++ src/test/java/org/devzendo/xpfsa/TestMacOSXFileMetadata.java
@@ -28,7 +28,9 @@ import org.devzendo.commoncode.logging.LoggingUnittestHelper;
 import org.devzendo.commoncode.os.OSTypeDetect;
 import org.devzendo.commoncode.os.OSTypeDetect.OSType;
 import org.devzendo.xpfsa.impl.MacOSXFileStatusImpl;
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -60,17 +62,20 @@ public final class TestMacOSXFileMetadata {
     @Rule
     public final TemporaryFolder tempDir = new TemporaryFolder();
     private String mTestDir;
+    private FileSystemAccess mFsa;
 
     /**
      * Create a temporary file to hold prefs data, that's deleted after
      * the JVM exits.
      * 
      * @throws IOException on failure
+     * @throws FileSystemAccessException 
      */
     @Before
-    public void setUp() throws IOException {
+    public void setUp() throws IOException, FileSystemAccessException {
         Assume.assumeTrue(osType == OSType.MacOSX);
-
+        LoggingUnittestHelper.setupLogging();
+        
         tempDir.create();
         mTestDir = tempDir.getRoot().getAbsolutePath();
         final IteratorExecutor it = new IteratorExecutor(new String[] 
@@ -80,22 +85,63 @@ public final class TestMacOSXFileMetadata {
         }
         final int exitValue = it.getExitValue();
         assertThat(exitValue, equalTo(0));
+
+        mFsa = new FileSystemAccess();
     }
     
     @Test
     public void directoryOwnershipObtained() throws IOException, FileSystemAccessException {
+        final File directory = new File(mTestDir);
+        assertThat("test data dir " + directory.getAbsolutePath() + " does not exist", directory.exists(), equalTo(true));
+
         final File testDir = new File(mTestDir, "directory");
-        assertThat("'directory' does not exist", testDir.exists(), equalTo(true));
+        assertThat("'directory' (" + testDir.getAbsolutePath() + ") does not exist", testDir.exists(), equalTo(true));
         
-        final FileSystemAccess fsa = new FileSystemAccess();
-        final DetailedFile detailedFile = fsa.getDetailedFile(testDir);
+        final DetailedFile detailedFile = mFsa.getDetailedFile(testDir);
         final FileStatus fs = detailedFile.getFileStatus();
         
         assertThat(fs, instanceOf(MacOSXFileStatus.class));
         final MacOSXFileStatus mfs = (MacOSXFileStatus) fs;
         assertThat(mfs.isDirectory(), equalTo(true));
+        assertThat(mfs.isRegularFile(), equalTo(false));
         assertThat(mfs.getPermissions(), equalTo(0755));
         assertThat(mfs.getUserID(), equalTo(TEST_UID));
         assertThat(mfs.getGroupID(), equalTo(TEST_GID));
     }
+
+    @Test
+    public void fileStatusObtained() throws FileSystemAccessException {
+        final File directory = new File(mTestDir);
+        assertThat("test data dir " + directory.getAbsolutePath() + " does not exist", directory.exists(), equalTo(true));
+
+        final File f = new File(mTestDir, "testfile");
+        assertThat(f.exists(), equalTo(true));
+        final DetailedFile df = mFsa.getDetailedFile(f);
+        final FileStatus fs = df.getFileStatus();
+        assertThat(fs, instanceOf(MacOSXFileStatus.class));
+        final MacOSXFileStatus mfs = (MacOSXFileStatus) fs;
+        LOGGER.info(mfs);
+        assertThat(mfs.getPermissions(), equalTo(0644));
+        assertThat(mfs.isRegularFile(), equalTo(true));
+        assertThat(mfs.getUserID(), equalTo(TEST_UID));
+        assertThat(mfs.getGroupID(), equalTo(TEST_GID));
+        assertThat(mfs.getNumberOfLinks(), equalTo(1));
+        assertThat(mfs.getSize(), equalTo(6L));
+    }
+
+    @Test
+    public void shouldFailToGetFileStatusForNonexistentFile() {
+        try {
+            final DetailedFile f = mFsa.getDetailedFile(new File("doesnotexist"));
+            f.getFileStatus();
+            final String msg = "Did not fail to get FileStatus for a file that doesn't exist";
+            LOGGER.warn(msg);
+            Assert.fail(msg);
+        } catch (final FileSystemAccessException e) {
+            final String msg = "Correct error from stat of nonexistent file: " + e.getMessage();
+            LOGGER.info(msg);
+            assertThat(e.getMessage(), equalTo("No such file or directory"));
+        }
+    }
+
 }
--- src/test/sh/setupmacosxtestdata.sh
+++ src/test/sh/setupmacosxtestdata.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
 TESTDIR=$1
-echo creating test dirrectory $TESTDIR
-mkdir $TESTDIR/directory
+if [ ! -d $TESTDIR ]; then
+    echo Test data directory $TESTDIR does not exist
+    exit 1
+fi
+
+echo populating test directory $TESTDIR
+cd $TESTDIR
+mkdir directory
+
+echo "hello" > testfile
+sleep 2
+chmod 644 testfile
+
+ln -s testfile link-to-testfile
+
+# small tree
+mkdir tree
+echo "firstfile" >> tree/firstfile
+echo "secondfile" >> tree/secondfile
+mkdir tree/onedir
+echo "onedir-firstfile" >> tree/onedir/onedir-firstfile
+echo "onedir-secondfile" >> tree/onedir/onedir-secondfile
+mkdir tree/twodir
+echo "twodir-firstfile" >> tree/twodir/twodir-firstfile
+echo "twodir-secondfile" >> tree/twodir/twodir-secondfile
+mkdir tree/twodir/bottomdir
+echo "twodir-bottomdir-firstfile" >> tree/twodir/bottomdir/twodir-bottomdir-firstfile
+echo "twodir-bottomdir-secondfile" >> tree/twodir/bottomdir/twodir-bottomdir-secondfile
+
+exit 0
