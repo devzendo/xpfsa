3e7527f35b23e1d878a12018ac0d93fcf06b6e1f
Matt Gumbley <matt.gumbley@devzendo.org>
2011-09-13 08:32:52 +0100

    Changed inline comments on the bitfield constants to JavaDoc.

--- src/main/java/org/devzendo/xpfsa/UnixFileStatus.java
+++ src/main/java/org/devzendo/xpfsa/UnixFileStatus.java
@@ -16,29 +16,98 @@
 package org.devzendo.xpfsa;
 
 public interface UnixFileStatus extends FileStatus {
-    public static final int S_IFMT = 0170000; //   bitmask for the file type bitfields
-    public static final int S_IFSOCK = 0140000; //   socket
-    public static final int S_IFLNK = 0120000; //   symbolic link
-    public static final int S_IFREG = 0100000; //   regular file
-    public static final int S_IFBLK = 0060000; //   block device
-    public static final int S_IFDIR = 0040000; //   directory
-    public static final int S_IFCHR = 0020000; //   character device
-    public static final int S_IFIFO = 0010000; //   FIFO
-    public static final int S_ISUID = 0004000; //   set UID bit
-    public static final int S_ISGID = 0002000; //   set-group-ID bit (see below)
-    public static final int S_ISVTX = 0001000; //   sticky bit (see below)
-    public static final int S_IRWXU = 00700; //   mask for file owner permissions
-    public static final int S_IRUSR = 00400; //   owner has read permission
-    public static final int S_IWUSR = 00200; //   owner has write permission
-    public static final int S_IXUSR = 00100; //   owner has execute permission
-    public static final int S_IRWXG = 00070; //   mask for group permissions
-    public static final int S_IRGRP = 00040; //   group has read permission
-    public static final int S_IWGRP = 00020; //   group has write permission
-    public static final int S_IXGRP = 00010; //   group has execute permission
-    public static final int S_IRWXO = 00007; //   mask for permissions for others (not in group)
-    public static final int S_IROTH = 00004; //   others have read permission
-    public static final int S_IWOTH = 00002; //   others have write permission
-    public static final int S_IXOTH = 00001; //   others have execute permission
+    /**
+     * Bitmask for the file type bitfields
+     */
+    public static final int S_IFMT = 0170000;
+    /**
+     * Bitfield representation for a socket
+     */
+    public static final int S_IFSOCK = 0140000;
+    /**
+     * Bitfield representation for a symbolic link
+     */
+    public static final int S_IFLNK = 0120000;
+    /**
+     * Bitfield representation for a regular file
+     */
+    public static final int S_IFREG = 0100000;
+    /**
+     * Bitfield representation for a block device
+     */
+    public static final int S_IFBLK = 0060000;
+    /**
+     * Bitfield representation for a directory
+     */
+    public static final int S_IFDIR = 0040000;
+    /**
+     * Bitfield representation for a character device
+     */
+    public static final int S_IFCHR = 0020000;
+    /**
+     * Bitfield representation for a FIFO
+     */
+    public static final int S_IFIFO = 0010000;
+    /**
+     * Bitfield representation for set UID bit
+     */
+    public static final int S_ISUID = 0004000;
+    /**
+     * Bitfield representation for set-group-ID bit
+     */
+    public static final int S_ISGID = 0002000;
+    /**
+     * Bitfield representation for sticky bit
+     */
+    public static final int S_ISVTX = 0001000;
+    /**
+     * Bitfield representation for mask for file owner permissions
+     */
+    public static final int S_IRWXU = 00700;
+    /**
+     * Bitfield representation for owner has read permission
+     */
+    public static final int S_IRUSR = 00400;
+    /**
+     * Bitfield representation for owner has write permission
+     */
+    public static final int S_IWUSR = 00200;
+    /**
+     * Bitfield representation for owner has execute permission
+     */
+    public static final int S_IXUSR = 00100;
+    /**
+     * Bitfield representation for mask for group permissions
+     */
+    public static final int S_IRWXG = 00070;
+    /**
+     * Bitfield representation for group has read permission
+     */
+    public static final int S_IRGRP = 00040;
+    /**
+     * Bitfield representation for group has write permission
+     */
+    public static final int S_IWGRP = 00020;
+    /**
+     * Bitfield representation for group has execute permission
+     */
+    public static final int S_IXGRP = 00010;
+    /**
+     * Bitfield representation for mask for permissions for others (not in group)
+     */
+    public static final int S_IRWXO = 00007;
+    /**
+     * Bitfield representation for others have read permission
+     */
+    public static final int S_IROTH = 00004;
+    /**
+     * Bitfield representation for others have write permission
+     */
+    public static final int S_IWOTH = 00002;
+    /**
+     * Bitfield representation for others have execute permission
+     */
+    public static final int S_IXOTH = 00001;
 
     boolean isSocket();
 
